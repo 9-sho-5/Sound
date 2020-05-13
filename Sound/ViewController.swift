6f7618427ab90b187a2ce27e58ac7d73db83bdc6
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-13 12:10:50 +0900

    first commit

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import AVFoundation     //音声動画の読み込み
 
 class ViewController: UIViewController {
 
+    let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapDrumButton () {
+        drumSoundPlayer.currentTime = 0
+        
+        drumSoundPlayer.play()
+    }
 
 }
 
